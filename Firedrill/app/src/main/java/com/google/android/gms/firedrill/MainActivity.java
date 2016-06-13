73aa9c2d068830af0271940f29a21cefb7739301
Shai Barack <shiber@gmail.com>
2016-06-13 13:44:18 +0100

    Can join to go to release eng screen Initial seek bar behavior

--- Firedrill/app/src/main/java/com/google/android/gms/firedrill/MainActivity.java
+++ Firedrill/app/src/main/java/com/google/android/gms/firedrill/MainActivity.java
@@ -1,7 +1,11 @@
 package com.google.android.gms.firedrill;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+        TextView gameId = (TextView) findViewById(R.id.game_id);
+        Button join = (Button) findViewById(R.id.join);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ReleaseEngActivity.class));
+            }
+        });
     }
 
 }
