87b2d5fb1eaeadcc44c0684a42207e4ba238861a
Shai Barack <shiber@gmail.com>
2016-06-13 14:04:07 +0100

    ClientTeam

---
+++ Firedrill/app/src/main/java/com/google/android/gms/firedrill/ClientTeam.java
@@ -0,0 +1,16 @@
+package com.google.android.gms.firedrill;
+
+/**
+ * Created by shayba on 6/13/16.
+ */
+
+public class ClientTeam {
+
+    public final String name;
+    public final boolean shippable;
+
+    public ClientTeam(String name, boolean shippable) {
+        this.name = name;
+        this.shippable = shippable;
+    }
+}
