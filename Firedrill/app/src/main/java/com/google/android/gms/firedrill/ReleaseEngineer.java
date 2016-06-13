c141b6343ddc33be7263859f93c4a3bd72936602
Mads Ager <ager@google.com>
2016-06-13 14:29:08 +0100

    Initial database setup code.

---
+++ Firedrill/app/src/main/java/com/google/android/gms/firedrill/ReleaseEngineer.java
@@ -0,0 +1,12 @@
+package com.google.android.gms.firedrill;
+
+/**
+ * Created by ager on 6/13/16.
+ */
+public class ReleaseEngineer  {
+    public int rolloutPercentage = 0;
+
+    public ReleaseEngineer() {
+
+    }
+}
---
+++ Firedrill/app/src/main/java/com/google/android/gms/firedrill/SoftwareEngineer.java
@@ -0,0 +1,12 @@
+package com.google.android.gms.firedrill;
+
+/**
+ * Created by ager on 6/13/16.
+ */
+public class SoftwareEngineer {
+    public int codingPercentage = 0;
+
+    public SoftwareEngineer() {
+
+    }
+}
